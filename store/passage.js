5326ef5e34930eb08dfae03447fb7f65c889c2a3
Wei <turbozv@gmail.com>
2017-04-16 00:57:27 -0700

    Use AsyncStorage to store bible verse cache

--- store/passage.js
+++ store/passage.js
@@ -1,12 +1,11 @@
+import { AsyncStorage } from 'react-native'
+
 // ------------------------------------
 // Constants
 // ------------------------------------
 export const REQUEST_PASSAGE = 'REQUEST_PASSAGE'
 export const RECEIVE_PASSAGE = 'RECEIVE_PASSAGE'
 export const FAILURE_PASSAGE = 'FAILURE_PASSAGE'
-
-// TODO: [Wei] Implement a global cache
-var localCache = []
 
 // Build the web service url
 function getUrl(book, verse) {
@@ -43,36 +42,36 @@ export function requestPassage (book, verse, navigator) {
       payload: { book, verse }
     })
 
-    let cache = localCache[book + ':' + verse]
-    if (cache != undefined) {
-      console.log("Cache_Hit:" + book + ':' + verse)
-      navigateToBibleScreen(dispatch, navigator, book, verse, cache)
-      return
-    }
-
-    // fetch bible verse from web service
-    fetch(getUrl(book, verse))
-      .then((response) => {
-        // FIXME: [Wei] "response.json()" triggers error on Android, so I have to use "eval"
-        return eval("(" + response._bodyText + ")")
-      })
-      .then((responseJson) => {
-        console.log("WebService returns: " + JSON.stringify(responseJson))
-        if (responseJson.error != undefined)
-        {
-          alert(responseJson.error)    
-          console.warn(responseJson.error)
-          return
-        }
-
-        localCache[book + ':' + verse] = responseJson
-        console.log("Cache_Set:" + book + ':' + verse)
-        navigateToBibleScreen(dispatch, navigator, book, verse, responseJson)
-      })
-      .catch((error) => {
-        alert(error)
-        console.warn(error)
-      });
+    let key = book + ':' + verse
+    AsyncStorage.getItem(key).then((value) => {
+      if (value != null) {
+        console.log("[Cache]Hit:" + book + ':' + verse)
+        navigateToBibleScreen(dispatch, navigator, book, verse, eval("(" + value + ")"))
+      } else {
+        console.log("[Cache]Miss:"+key)
+        // fetch bible verse from web service
+        fetch(getUrl(book, verse))
+          .then((response) => {
+            // FIXME: [Wei] "response.json()" triggers error on Android, so I have to use "eval"
+            return eval("(" + response._bodyText + ")")
+          })
+          .then((responseJson) => {
+            console.log("WebService returns: " + JSON.stringify(responseJson))
+            if (responseJson.error != undefined) {
+              alert(responseJson.error)    
+              console.warn(responseJson.error)
+            } else {
+              AsyncStorage.setItem(key, JSON.stringify(responseJson))
+              console.log("[Cache]Set:" + key)
+              navigateToBibleScreen(dispatch, navigator, book, verse, responseJson)
+            }
+          })
+          .catch((error) => {
+            alert(error)
+            console.warn(error)
+          });
+      }
+    }).done();
   }
 }
 
