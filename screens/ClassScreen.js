a5549363b929a5979aee6e119d500661a6ec5996
xiaoh <xiaoh@microsoft.com>
2017-04-24 23:57:18 -0700

    fix Android keyboard scroll

--- screens/ClassScreen.js
+++ screens/ClassScreen.js
@@ -30,19 +30,9 @@ class ClassScreen extends React.Component {
     const requestPassage = (book, verse) => {
       this.props.requestPassage(book, verse, this.props.navigator)
     }
-    let content = (
-      <ScrollableTabView initialPage={1}>
-        <NotesPage tabLabel="讲义" />
-        <DayQuestions tabLabel="一" requestPassage={requestPassage} day={this.props.dayQuestions.one} readVerse={this.props.dayQuestions.one.readVerse} memoryVerse={this.props.memoryVerse} />
-        <DayQuestions tabLabel="二" requestPassage={requestPassage} day={this.props.dayQuestions.two} readVerse={this.props.dayQuestions.two.readVerse} />
-        <DayQuestions tabLabel="三" requestPassage={requestPassage} day={this.props.dayQuestions.three} readVerse={this.props.dayQuestions.three.readVerse} />
-        <DayQuestions tabLabel="四" requestPassage={requestPassage} day={this.props.dayQuestions.four} readVerse={this.props.dayQuestions.four.readVerse} />
-        <DayQuestions tabLabel="五" requestPassage={requestPassage} day={this.props.dayQuestions.five} readVerse={this.props.dayQuestions.five.readVerse} />
-        <DayQuestions tabLabel="六" requestPassage={requestPassage} day={this.props.dayQuestions.six} readVerse={this.props.dayQuestions.six.readVerse} />
-      </ScrollableTabView>
-    )
 
-    return (
+    // TODO:[Wei] KeyboardAwareScrollView works on iOS but not Android, KeyboardAvoidingView works on Android, but not iOS :(
+    return (Platform.OS === 'ios') ? (
       <ScrollableTabView initialPage={1}>
         <NotesPage tabLabel="讲义" />
         <DayQuestions tabLabel="一" requestPassage={requestPassage} day={this.props.dayQuestions.one} readVerse={this.props.dayQuestions.one.readVerse} memoryVerse={this.props.memoryVerse} />
@@ -52,7 +42,19 @@ class ClassScreen extends React.Component {
         <DayQuestions tabLabel="五" requestPassage={requestPassage} day={this.props.dayQuestions.five} readVerse={this.props.dayQuestions.five.readVerse} />
         <DayQuestions tabLabel="六" requestPassage={requestPassage} day={this.props.dayQuestions.six} readVerse={this.props.dayQuestions.six.readVerse} />
       </ScrollableTabView>
-    );
+    ) : (
+        <KeyboardAvoidingView style={styles.container} behavior='padding' keyboardVerticalOffset={80}>
+          <ScrollableTabView initialPage={1}>
+            <NotesPage tabLabel="讲义" />
+            <DayQuestions tabLabel="一" requestPassage={requestPassage} day={this.props.dayQuestions.one} readVerse={this.props.dayQuestions.one.readVerse} memoryVerse={this.props.memoryVerse} />
+            <DayQuestions tabLabel="二" requestPassage={requestPassage} day={this.props.dayQuestions.two} readVerse={this.props.dayQuestions.two.readVerse} />
+            <DayQuestions tabLabel="三" requestPassage={requestPassage} day={this.props.dayQuestions.three} readVerse={this.props.dayQuestions.three.readVerse} />
+            <DayQuestions tabLabel="四" requestPassage={requestPassage} day={this.props.dayQuestions.four} readVerse={this.props.dayQuestions.four.readVerse} />
+            <DayQuestions tabLabel="五" requestPassage={requestPassage} day={this.props.dayQuestions.five} readVerse={this.props.dayQuestions.five.readVerse} />
+            <DayQuestions tabLabel="六" requestPassage={requestPassage} day={this.props.dayQuestions.six} readVerse={this.props.dayQuestions.six.readVerse} />
+          </ScrollableTabView>
+        </KeyboardAvoidingView>
+      );
   }
 }
 
@@ -99,19 +101,16 @@ const DayQuestions = (props) => {
     </View>
   );
 
-  // TODO:[Wei] KeyboardAwareScrollView works on iOS but not Android, KeyboardAvoidingView works on Android, but not iOS :(
   // TODO: Need verify if this new KeyboardAwareScrollView position can work on Android
   return (Platform.OS === 'ios') ? (
     <KeyboardAwareScrollView style={styles.dayQuestionsContainer} extraScrollHeight={80}>
       {content}
     </KeyboardAwareScrollView>
   ) : (
-    <KeyboardAvoidingView style={styles.container} behavior='padding' keyboardVerticalOffset={80}>
       <ScrollView style={styles.dayQuestionsContainer}>
         {content}
       </ScrollView>
-    </KeyboardAvoidingView>
-  );
+    );
 }
 
 const BSFQuestion = (props) => (
