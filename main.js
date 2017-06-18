099caa9ca10603ebcd27f41bbad7eeb765caa6cb
Wei Zhang <turbozv@gmail.com>
2017-06-17 17:00:11 -0700

    Logon screen initial check-in

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { NavigationProvider, StackNavigation } from '@expo/ex-navigation';
 import { FontAwesome } from '@expo/vector-icons';
 import { Provider } from 'react-redux';
-
+import Layout from './constants/Layout';
 import createStore from './store/createStore'
 import Router from './navigation/Router';
 import cacheAssetsAsync from './utilities/cacheAssetsAsync';
 import { loadAsync } from './dataStorage/storage';
 import { Models } from './dataStorage/models';
+import LoginUI from './components/LoginUI';
 
 let store;
 
 class AppContainer extends React.Component {
   state = {
     appIsReady: false,
+    userIsLoggedOn: false,
   };
 
   componentWillMount() {
@@ -29,7 +31,10 @@ class AppContainer extends React.Component {
         loadAsync(Models.Book, "home.json", true),
         loadAsync(Models.Answer, null, true),
         cacheAssetsAsync({
-          images: [require('./assets/images/expo-wordmark.png')],
+          images: [
+            require('./assets/images/expo-wordmark.png'),
+            require('./assets/icons/app-icon.png')
+          ],
           fonts: [
             FontAwesome.font,
             { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
@@ -44,7 +49,8 @@ class AppContainer extends React.Component {
       store = createStore(initialstate);
 
       // prefetch data here:
-
+      // TODO:[Wei] Check if the user has already logged on, set userIsLoggedOn = true
+      
       // set the app status to ready
       this.setState({ appIsReady: true });
     } catch (err) {
@@ -52,23 +58,40 @@ class AppContainer extends React.Component {
     }
   }
 
+  onUserLogon(data) {
+    console.log(JSON.stringify(data));
+    if (data.logon) {
+      this.setState({ userIsLoggedOn: true });
+    }
+  }
+
   render() {
     if (this.state.appIsReady) {
-      return (
-        <Provider store={store}>
-          <View style={styles.container}>
-            <NavigationProvider router={Router}>
-              <StackNavigation
-                id="root"
-                initialRoute={Router.getRoute('rootNavigation')}
-              />
-            </NavigationProvider>
+      if (this.state.userIsLoggedOn) {
+        return (
+          <Provider store={store}>
+            <View style={styles.container}>
+              <NavigationProvider router={Router}>
+                <StackNavigation
+                  id="root"
+                  initialRoute={Router.getRoute('rootNavigation')}
+                />
+              </NavigationProvider>
 
-            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-            {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
-          </View>
-        </Provider>
-      );
+              {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+              {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
+            </View>
+          </Provider>
+        );
+      } else {
+        return (
+          <Provider store={store}>
+            <View style={styles.loginView}>
+              <LoginUI onUserLogon={this.onUserLogon.bind(this)} />
+            </View>
+          </Provider>
+        );
+      }
     } else {
       return <Expo.AppLoading />;
     }
@@ -84,6 +107,11 @@ const styles = StyleSheet.create({
     height: 24,
     backgroundColor: 'rgba(0,0,0,0.2)',
   },
+  loginView: {
+    width: Layout.window.width,
+    height: Layout.window.height,
+    marginTop: 50
+  }
 });
 
 Expo.registerRootComponent(AppContainer);
