788d70a2384a47263894420cb634a874b809b1ce
Wei Zhang <turbozv@gmail.com>
2017-11-20 17:50:17 -0800

    Disable push notification

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -36,11 +36,11 @@ const RootStackNavigator = StackNavigator(
 
 export default class RootNavigator extends React.Component {
   componentDidMount() {
-    this._notificationSubscription = this._registerForPushNotifications();
+    //this._notificationSubscription = this._registerForPushNotifications();
   }
 
   componentWillUnmount() {
-    this._notificationSubscription && this._notificationSubscription.remove();
+    //this._notificationSubscription && this._notificationSubscription.remove();
   }
 
   render() {
