553a908268fb9666fb25cd4d7d3ed151287d4322
Wei Zhang <turbozv@gmail.com>
2018-01-20 17:35:51 -0800

    Support attendance

--- screens/AttendanceScreen.js
+++ screens/AttendanceScreen.js
@@ -3,48 +3,87 @@ import { ScrollView, StyleSheet, View, Alert, Text } from 'react-native';
 import { getI18nText } from '../store/I18n';
 import { getCurrentUser } from '../store/user';
 import { CheckBox, Button, SearchBar, Grid, Col, List, ListItem } from 'react-native-elements';
+import Layout from '../constants/Layout';
+import { Models } from '../dataStorage/models';
+import { callWebServiceAsync, showWebServiceCallErrorsAsync } from '../dataStorage/storage';
 
 export default class AttendanceScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
-      title: getI18nText('考勤表')
+      title: getI18nText('考勤表') + '  (' + navigation.state.params.data.date + ')'
     };
   };
 
   state = {
-    userData: this.props.navigation.state.params.data
+    classDate: this.props.navigation.state.params.data.date,
+    users: this.props.navigation.state.params.data.attendees
   }
 
   async onSubmit() {
-    this.props.navigation.goBack();
+    let users = [];
+    for (var i in this.state.users) {
+      if (this.state.users[i].checked) {
+        users.push(this.state.users[i].id);
+      }
+    }
+
+    let body = { date: this.state.classDate, users };
+    console.log(JSON.stringify(body));
+
+    const result = await callWebServiceAsync(Models.Attendance.restUri, '?cellphone=' + phone, 'POST', [], body);
+    const succeed = await showWebServiceCallErrorsAsync(result, 201);
+    if (succeed) {
+      Alert.alert(getI18nText('已经提交，谢谢！'));
+    }
+  }
+
+  onCheck(user) {
+    const newValue = user.checked ? false : true;
+    users = this.state.users
+    for (var i in users) {
+      if (users[i].id == user.id) {
+        users[i].checked = newValue;
+        break;
+      }
+    }
+
+    this.setState({ users });
   }
 
   render() {
     let keyIndex = 0;
     return (
-      <ScrollView
-        style={{ backgroundColor: 'white' }}
-        ref={ref => this.scrollView = ref}>
-        <View style={{ backgroundColor: 'white' }}>
-          <Text>{this.state.userData.date}</Text>
-          <View style={{ alignItems: 'center', marginTop: 40 }}>
+      <View style={{ flex: 1 }}>
+        <ScrollView
+          style={{ backgroundColor: 'white' }}
+          ref={ref => this.scrollView = ref}>
+          <View style={{ alignItems: 'center', marginTop: 10, marginBottom: 50 }}>
             {
-              this.state.userData.attendees.map((user) => (
+              this.state.users.map((user) => (
                 <CheckBox
+                  containerStyle={{ width: Layout.window.width - 20 }}
                   key={keyIndex++}
-                  title={user.id + ':' + user.name + ' ' + user.cellphone}
-                  checked={user.checked} />
+                  title={`#${keyIndex}: ${user.name} (${user.cellphone})`}
+                  checked={user.checked}
+                  onPress={() => { this.onCheck(user) }} />
               ))
             }
-            <Button
-              backgroundColor='#397EDC'
-              borderRadius={5}
-              containerViewStyle={{ width: 130 }}
-              title={getI18nText('提交')}
-              onPress={this.onSubmit.bind(this)} />
           </View>
+        </ScrollView>
+        <View style={{
+          position: 'absolute',
+          bottom: 5,
+          width: Layout.window.width,
+          alignItems: 'center'
+        }}>
+          <Button
+            backgroundColor='#397EDC'
+            borderRadius={5}
+            containerViewStyle={{ width: 130 }}
+            title={getI18nText('提交')}
+            onPress={this.onSubmit.bind(this)} />
         </View>
-      </ScrollView>
+      </View>
     );
   }
 }
